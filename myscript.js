d994dbc7eb7ec29e260ecec2eb0e0885051521fa
demi <96106786+demejan@users.noreply.github.com>
2023-04-16 07:07:04 +0600

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var name = prompt("enter name")
+var age = prompt("enter age")
+
+if (age > 18) {output1 = "you are older than 18}
+else {output1 = "you are younger than 18")
+
+alert(name, output1)
