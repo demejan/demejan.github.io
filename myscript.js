4da0d881bf68fc94c6564dacd45a13720a519ea6
demi <96106786+demejan@users.noreply.github.com>
2023-04-16 14:11:27 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,9 @@
-var name = prompt("enter name")
-var age = prompt("enter age")
+function MyButton(){    
+    var name = prompt("enter name")
+    var age = prompt("enter age")
 
-if (age > 18) {output1 = "you are older than 18}
-else {output1 = "you are younger than 18")
+    if (age > 18) {output1 = " you are older than 18"
+    }else {output1 = " you are younger than 18"}
 
-alert(name, output1)
+    alert(name+ output1)
+}
